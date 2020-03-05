61c0d85fb7df42639cd6b943ca30b288110cd823
Mistico <clementmistico@gmail.com>
2020-03-05 16:34:13 +0200

    chore(host):host backend to heroku

--- src/constants/actionTypes.js
+++
@@ -1,6 +0,0 @@
-// action types here
-export const SIGNOUT = 'SIGNOUT';
-export const LEAVE = 'LEAVE';
-export const JOIN = 'JOIN';
-export const VIEW_CONNECTED_USERS = 'VIEW_CONNECTED_USERS';
-export const SUCCESS_LOGIN = 'SUCCESS_LOGIN';
--- src/redux/reducers/rootReducer.js
+++
@@ -1,7 +0,0 @@
-// root reducer codes
-import { combineReducers } from 'redux';
-import auth from './authReducer'
-
-const rootReducer = combineReducers({auth});
-
-export default rootReducer;

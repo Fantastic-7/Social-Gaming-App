41c510a93476187f030ac8a05fa2dbe0c7482625
Elvis-rugamba <elvisrugamba@gmail.com>
2020-03-09 13:39:49 +0200

    game over

--- src/redux/middlewares/socketMiddleware.js
+++ src/redux/middlewares/socketMiddleware.js
@@ -24,6 +24,7 @@ const socketMiddleware = ({ dispatch }) => next => action => {
 		socket.emit('done', action.payload[0]);
 		socket.on('show_score', scores => {
 			dispatch(updateScoreBoard());
+			console.log(scores);
 			dispatch(showScore(scores));
 		});
 	}
